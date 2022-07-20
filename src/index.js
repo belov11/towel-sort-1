336f2f6a792d45d785dce4247614c18e63abfd9d
belov11 <pasha_belov_96@mail.ru>
2022-07-20 10:01:02 +0300

    feat: add code index js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix === undefined ? [] : matrix.reduce((res, item, index) => index % 2 === 0 ? res.concat(item) : res.concat(item.reverse()), []);
 }
